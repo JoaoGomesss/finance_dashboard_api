66ec1afdc1b0fdd7ef628b4f25429cfb744d5ea8
Joao Gomes <jowgomes3@gmail.com>
2024-10-16 13:49:26 -0300

    feat: add CreateTransactionController factory

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,3 +2,5 @@ export * from '../controller/user/create-user.js'
 export * from '../controller/user/get-user-by-id.js'
 export * from '../controller/user/update-user.js'
 export * from '../controller/user/delete-user.js'
+
+export * from '../controller/transaction/create-transaction.js'
