065648efaba84342cd117ac6fef88ceeadc20372
Joao Gomes <jowgomes3@gmail.com>
2024-10-22 18:52:14 -0300

    feat: add route to update a transaction

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -4,5 +4,5 @@ export * from './user/get-user-by-id.js'
 export * from './user/delete-user.js'
 
 export * from './transactions/create-transaction.js'
-
 export * from './transactions/get-transactions-by-user-id.js'
+export * from './transactions/update-transaction.js'
