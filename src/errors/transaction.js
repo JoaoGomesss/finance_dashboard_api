6bc1a11fc6f69962ba0c2c00e78e47f5092496d5
João Gomes <jowgomes3@gmail.com>
2024-10-31 09:08:45 -0300

    feat: ensure CreateTransaction and DeleteTransactionRepository use Prisma

--- src/errors/transaction.js
+++
@@ -1,6 +0,0 @@
-export class TransactionNotFoundError extends Error {
-    constructor(transactionId) {
-        super(`Transaction with id ${transactionId} was not found.`)
-        this.name = 'TransactionNotFoundError'
-    }
-}
--- src/repositories/postgres/transactions/delete-transaction.js
+++ src/repositories/postgres/transactions/delete-transaction.js
@@ -1,6 +1,4 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
 import { prisma } from '../../../../prisma/prisma.js'
-import { TransactionNotFoundError } from '../../../errors/index.js'
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
@@ -11,13 +9,8 @@ export class PostgresDeleteTransactionRepository {
                 },
             })
         } catch (error) {
-            if (error instanceof PrismaClientKnownRequestError) {
-                if (error.code === 'P2025') {
-                    throw new TransactionNotFoundError(transactionId)
-                }
-            }
-
-            throw error
+            console.error(error)
+            return null
         }
     }
 }
