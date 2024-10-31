e551265ecc74b58f03b34795ec257002313dff27
João Gomes <jowgomes3@gmail.com>
2024-10-31 09:22:19 -0300

    feat: ensure Update and GetTransactionsByUserIdRepository use Prisma

--- src/controller/transaction/delete-transaction.js
+++ src/controller/transaction/delete-transaction.js
@@ -1,38 +1,26 @@
-import { TransactionNotFoundError } from '../../errors/transaction.js'
 import {
     checkIfIdIsValid,
     invalidIdResponse,
     ok,
     serverError,
-    transactionNotFoundResponse,
 } from '../helpers/index.js'
 
 export class DeleteTransactionController {
     constructor(deleteTransactionUseCase) {
         this.deleteTransactionUseCase = deleteTransactionUseCase
     }
-
     async execute(httpRequest) {
         try {
             const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
-
             if (!idIsValid) {
                 return invalidIdResponse()
             }
-
-            const deletedTransaction =
-                await this.deleteTransactionUseCase.execute(
-                    httpRequest.params.transactionId,
-                )
-
-            return ok(deletedTransaction)
+            const transaction = await this.deleteTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+            )
+            return ok(transaction)
         } catch (error) {
-            if (error instanceof TransactionNotFoundError) {
-                return transactionNotFoundResponse()
-            }
-
             console.error(error)
-
             return serverError()
         }
     }
