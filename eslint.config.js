dd8867de3466ff32c9173ef4bb2747df46042eed
João Gomes <jowgomes3@gmail.com>
2024-11-01 11:01:36 -0300

    chore: add initial jest setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,14 @@
-import globals from 'globals'
+import globals, { node } from 'globals'
 import pluginJs from '@eslint/js'
 
 export default [
     { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
+    {
+        env: {
+            es2021: true,
+            node: true,
+            jest: true,
+        },
+    },
 ]
