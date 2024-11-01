6a8be5eb899901e34ed87ada3a1dbdc5cdef67bc
João Gomes <jowgomes3@gmail.com>
2024-11-01 13:32:30 -0300

    test: ensure 400 is returned if email is not provided

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -57,4 +57,20 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if email is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Jonh',
+                last_name: 'Doe',
+                password: '1233546423',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })
