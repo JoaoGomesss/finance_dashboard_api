48407d6e156b70130209f8e9fc9b52d0d717929c
João Gomes <jowgomes3@gmail.com>
2024-11-01 13:23:36 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -41,4 +41,20 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if last_name is not provided', async () => {
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Gomes',
+                email: 'jg@gmail.com',
+                password: '13465878',
+            },
+        }
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })
