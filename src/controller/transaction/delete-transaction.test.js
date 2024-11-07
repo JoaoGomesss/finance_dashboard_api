9171bf62154544e8957d19b9fce3efd28bfcd9f8
João Gomes <jowgomes3@gmail.com>
2024-11-07 16:16:30 -0300

    test: ensure DeleteTransactionController returns 200 when deleting a transaction successfully

---
+++ src/controller/transaction/delete-transaction.test.js
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker'
+import { DeleteTransactionController } from './delete-transaction'
+
+describe('DeleteTransactionController', () => {
+    class deleteTransactionUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EXPENSE',
+                amount: Number(faker.finance.amount()),
+            }
+        }
+    }
+    const makeSut = () => {
+        const deleteTransactionUseCase = new deleteTransactionUseCaseStub()
+        const sut = new DeleteTransactionController(deleteTransactionUseCase)
+
+        return { sut, deleteTransactionUseCase }
+    }
+
+    it('should return 200 when deleting a transaction successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            params: { transactionId: faker.string.uuid() },
+        })
+
+        expect(result.statusCode).toBe(200)
+    })
+})
