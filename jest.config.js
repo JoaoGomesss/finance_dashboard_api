cd5c8544cc672382de8830feb77b73d5077c5315
João Gomes <jowgomes3@gmail.com>
2024-11-20 17:26:56 -0300

    chore: remove useless .mjs extensions from jest config files

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,8 @@ const config = {
     collectCoverageFrom: ['src/**/*.js'],
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
-    globalSetup: '<rootDir>/jest.global-setup.mjs',
+    globalSetup: '<rootDir>/jest.global-setup.js',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup-after-env.js'],
 }
 
 export default config
