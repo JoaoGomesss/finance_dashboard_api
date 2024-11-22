d4c9fde2b608102a07c04f27b8d65469223f1454
João Gomes <jowgomes3@gmail.com>
2024-11-22 17:32:45 -0300

    ensure: DELETE /api/users/:userId returns 200 when user is deleted

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -48,4 +48,17 @@ describe('UserRoutesE2ETests', () => {
         expect(response.body.email).toBe(updateUserParams.email)
         expect(response.body.password).not.toBe(updateUserParams.password)
     })
+
+    it('DELETE /api/users/:userId should return 200 when user is deleted', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users/')
+            .send({ ...user, id: undefined })
+
+        const response = await request(app).delete(
+            `/api/users/${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(createdUser)
+    })
 })
