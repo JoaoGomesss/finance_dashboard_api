45bcb501e92dda74db80e2b54096f4396f5e6906
João Gomes <jowgomes3@gmail.com>
2024-11-22 15:46:31 -0300

    test: ensure PasswordHasherAdapter hashes a password

--- src/adapter/id-generator.test.js
+++ src/adapter/id-generator.test.js
@@ -2,10 +2,10 @@ import validator from 'validator'
 import { IdGeneratorAdapter } from './id-generator'
 
 describe('IdGeneratorAdapter', () => {
-    it('should return a random id', () => {
+    it('should return a random id', async () => {
         const sut = new IdGeneratorAdapter()
 
-        const result = sut.execute()
+        const result = await sut.execute()
 
         expect(result).toBeTruthy()
         expect(typeof result).toBe('string')
