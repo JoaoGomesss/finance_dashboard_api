9782c8ec031617d106d8fa08d1e4695e26bd874f
João Gomes <jowgomes3@gmail.com>
2024-11-24 18:04:42 -0300

    ci: fix test run on gh actions

--- src/routes/transaction.e2e.test.js
+++ src/routes/transaction.e2e.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { app } from '../app'
+import { app } from '../app.js'
 import { transaction, user } from '../tests'
 import { TransactionType } from '@prisma/client'
 
